4bd08b39b478525c86446343afbd477557a1722b
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-28 15:03:24 -0600

    Create movie-card.jsx

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class MovieCard extends React.Component {
+  render() {
+
+    const { movie } = this.props;
+
+    return (
+      <div className="movie-card">{movie.Title}</div>
+    )
+  }
+}
