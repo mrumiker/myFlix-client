df4237240411ed5934ae156acfa744fd88eac030
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-30 12:40:08 -0600

    Create login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    console.log(username, password);
+    // Send a req to the server for authentication
+    // then call props.onLoggedIn(username)  
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="button" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
