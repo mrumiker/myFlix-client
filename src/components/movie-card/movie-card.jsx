ba464ab43f1928ecbc414ae505e8415ac756714e
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-30 13:18:38 -0600

    add Prop types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -10,3 +11,23 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    }).isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
