21a1abcd664aa00d0972e22ffdb81145e30d5272
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-12-01 20:19:36 -0600

    add Bootstrap to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -14,29 +15,25 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div className="movie-view">
-        <img className="movie-poster" src={movie.ImagePath} />
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
+      <Card style={{ width: '16rem' }} className="movie-view">
+        <Card.Img className="movie-poster" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title className="movie-title">{movie.Title}</Card.Title>
 
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="back-button">
-          <button onClick={() => window.location.reload()}>Back</button>
-        </div>
-      </div>
+          <Card.Text className="movie-info">
+            {`Description: ${movie.Description}`}
+          </Card.Text>
+          <Card.Text className="movie-info">
+            {`Genre: ${movie.Genre.Name}`}
+          </Card.Text>
+          <Card.Text className="movie-info">
+            {`Director: ${movie.Director.Name}`}
+          </Card.Text>
+
+          <Button className="back-button" variant="secondary" onClick={() => window.location.reload()}>Back</Button>
+
+        </Card.Body>
+      </Card>
     );
   }
 }
